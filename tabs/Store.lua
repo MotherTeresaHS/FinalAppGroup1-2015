97710ced2936f3ae3d4ede07ae447ef3b5bdc693
AlexandraKuhn at codea-scm <AlexandraKuhn@codea-scm.aws.mapote.com>
2015-11-23 10:56:28 -0500

    Initial Commit

---
+++ tabs/Store.lua
@@ -0,0 +1,16 @@
+-- Store
+
+Store = class()
+
+function Store:init(x)
+    -- you can accept and set parameters here
+    self.x = x
+end
+
+function Store:draw()
+    -- Codea does not automatically call this method
+end
+
+function Store:touched(touch)
+    -- Codea does not automatically call this method
+end
